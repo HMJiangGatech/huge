64a0f9f45a41109f16d031f0c19db741f7f8a38f
HMJiangGatech <jianghm.ustc@gmail.com>
2018-08-14 03:20:45 -0400

    update

---
+++ R/huge.inference.R
@@ -0,0 +1,235 @@
+#-----------------------------------------------------------------------#
+# Package: High-dimensional Undirected Graph Estimation                 #
+# huge.inference(): graph inference                                     #
+# Authors: Tuo Zhao and Han Liu                                         #
+# Emails: <tzhao5@jhu.edu> and <hanliu@cs.jhu.edu>                      #
+# Date: Jul 15th 2011                                                   #
+# Version: 1.1.0                                                        #
+#-----------------------------------------------------------------------#
+
+#' Graph inference
+#' 
+#' Implements the inference for high dimensional graphical models, including Gaussian and Nonparanormal graphical models
+#' We consider the problems of testing the presence of a single edge and the hypothesis is that the edge is absent.
+#' 
+#' For Nonparanormal graphical model we provide Score test method and Wald Test. However it is really slow for inferencing on Nonparanormal model, especially for large data.
+#' 
+#'   
+#' @param data The input \code{n} by \code{d} data matrix(\code{n} is the sample size and \code{d} is the dimension).
+#' @param T The estimated inverse of correlation matrix of the data.
+#' @param adj The adjacency matrix corresponding to the graph.
+#' @param alpha The significance level of hypothesis.The default value is \code{0.05}.
+#' @param type The type of input data. There are 2 options: \code{"Gaussian"} and \code{"Nonparanormal"}. The defaulty value is \code{"Gaussian"}.
+#' @param method When using nonparanormal graphical model. Test method with 2 options: \code{"score"} and \code{"wald"}. The defaulty value is \code{"score"}.
+#' @seealso \code{\link{huge}}, and \code{\link{huge-package}}.
+#' @return 
+#' An object is returned:  
+#' \item{data}{
+#'   The \code{n} by \code{d} data matrix from the input.
+#' }
+#' \item{p}{
+#'   The \code{d} by \code{d} p-value matrix of hypothesis. 
+#' }
+#' \item{error}{
+#'   The type I error of hypothesis at alpha significance level.
+#' }
+#' @examples
+#' #generate data
+#' L = huge.generator(n = 50, d = 12, graph = "hub", g = 4)
+#' 
+#' #graph path estimation using glasso
+#' est = huge(L$data, method = "glasso")
+#' 
+#' #inference of Gaussian graphical model at 0.05 significance level
+#' T = tail(est$icov, 1)[[1]]
+#' out1 = huge.inference(L$data, T, L$theta)
+#' 
+#' #inference of Nonparanormal graphical model using score test at 0.05 significance level
+#' T = tail(est$icov, 1)[[1]]
+#' out2 = huge.inference(L$data, T, L$theta, type = "Nonparanormal")
+#' 
+#' #inference of Nonparanormal graphical model using wald test at 0.05 significance level
+#' T = tail(est$icov, 1)[[1]]
+#' out3 = huge.inference(L$data, T, L$theta, type = "Nonparanormal", method = "wald")
+#' 
+#' #inference of Nonparanormal graphical model using wald test at 0.1 significance level
+#' T = tail(est$icov, 1)[[1]]
+#' out4 = huge.inference(L$data, T, L$theta, 0.1, type = "Nonparanormal", method = "wald")
+#' @references 
+#' 1.Q Gu, Y Cao, Y Ning, H Liu. Local and global inference for high dimensional nonparanormal graphical models.\cr
+#' 2.J Jankova, S Van De Geer. Confidence intervals for high-dimensional inverse covariance estimation. \emph{Electronic Journal of Statistics}, 2015.\cr
+#' @export
+huge.inference = function(data, T, adj, alpha = 0.05, type = "Gaussian", method = "score"){
+  d = ncol(data)
+  n = nrow(data)
+
+  if(type == "Gaussian")
+  {
+    x=scale(data)
+    U=cor(x)
+    W<-2*T - T%*%U%*%T
+    W_n<-matrix(0,d,d)
+    sigma<-matrix(0,d,d)
+    for(j in 1:d)
+    {
+      for (k in 1:d)
+      {
+        sigma[j, k] = sqrt(T[j, j]*T[k, k]+T[j, k]^2)
+        W_n[j, k] = sqrt(n)*W[j, k]/sigma[j, k]
+      }
+    }
+    p<-matrix(0,d,d)
+    p<-apply(W_n, 1, function(x) 2*(1 - pnorm(abs(x))))
+    rm(sigma,W_n)
+  }
+  if(type == "Nonparanormal")
+  {
+    x=data
+    U<-matrix(0,d,d)
+    G=list()
+    Test<-matrix(0,d,d)
+    for(i in 1:n)
+      G[[i]]<-matrix(0,d,d)
+    Temp_jk<-matrix(0,n,n)
+    for(j in 1:d)
+    {
+      for(k in 1:d)
+      {
+        if(j==k)
+        {
+          U[j, k] = 1
+          next
+        }
+ 
+        for(i1 in 1:n)
+        {
+          for(i2 in 1:n)
+          {
+            Temp_jk[i1, i2] = sign((x[i1, j] - x[i2, j])*(x[i1, k] - x[i2, k]))
+            G[[i1]][j, k] = G[[i1]][j, k] - pi/2*Temp_jk[i1, i2]
+            
+          }
+        }
+        U[j, k] = sin(pi/2*sum(Temp_jk)/((n-1)*n))
+        for(i in 1:n)
+          G[[i]][j, k] = G[[i]][j, k]/(n-1) + asin(U[j, k])
+      }
+    }
+    #F
+    F<-apply(U,1,function(x) sqrt(1-x^2))
+    #R
+    R<-matrix(0,d^2,d^2)
+    for (i in 1:n)
+      R<-R + as.matrix(as.vector(F*G[[i]]))%*%as.vector(F*G[[i]])
+    R<-R/n
+    
+    #kronecker product of T
+    T_k<-kronecker(T, T)
+    
+    if(method == "score")
+    {
+      S<-matrix(0,d,d)
+      sigma<-matrix(0,d,d)
+      #ST_n
+      ST_n<-matrix(0,d,d)
+      for(j in 1:d)
+      {
+        for(k in 1:d)
+        {
+          #S
+          ej<-matrix(0,d,1)
+          ek<-matrix(0,d,1)
+          ej[j] = 1
+          ek[k] = 1
+          T_h<-T
+          T_h[j, k]=0
+          S[j, k] = t(ej)%*%t(T_h)%*%U%*%T_h%*%ek/(T[j, j]*T[k, k])
+          
+          #w
+          temp1<-T_k[,j*k]
+          temp1<-temp1[-j*k]
+          w<-as.matrix((-temp1)/T_k[j*k, j*k])
+          
+          #sigma
+          temp2<-R[j*k,]
+          temp2<-as.matrix(temp2[-j*k])
+          sigma[j, k] = sqrt(R[j*k, j*k] - 2*t(temp2)%*%w + t(w)%*%R[-j*k, -j*k]%*%w)
+          
+          ST_n[j, k] = S[j, k]*sqrt(n)/(2*sigma[j, k])
+          
+        }
+      }
+      
+      #p-value
+      p<-matrix(0,d,d)
+      p<-apply(ST_n, 1, function(x) 2*(1 - pnorm(abs(x))))
+      rm(temp1,temp2,ST_n,S)
+    }
+    
+    if(method == "wald")
+    {
+      T_W<-matrix(0,d,d)
+      sigma<-matrix(0,d,d)
+      #W_n
+      W_n<-matrix(0,d,d)
+      temp1<-T%*%U
+      temp2<-U%*%T
+      for(j in 1:d)
+      {
+        for(k in 1:d)
+        {
+          #w
+          temp3<-T_k[,j*k]
+          temp3<-temp3[-j*k]
+          w<-as.matrix((-temp3)/T_k[j*k,j*k])
+          
+          #sigma
+          temp4<-R[j*k,]
+          temp4<-as.matrix(temp4[-j*k])
+          sigma[j, k] = sqrt(R[j*k, j*k] - 2*t(temp4)%*%w + t(w)%*%R[-j*k, -j*k]%*%w)
+          
+          #T_W
+          ej<-matrix(0,d,1)
+          ek<-matrix(0,d,1)
+          ej[j] = 1
+          ek[k] = 1
+          T_W[j ,k] = (T[j,k]*t(ej)%*%temp1%*%ek + T[j, k]*t(ej)%*%temp2%*%ek - t(ej)%*%t(T)%*%temp2%*%ek)/(t(ej)%*%temp1%*%ek + t(ej)%*%temp2%*%ek - 1)
+          W_n[j, k] = T_W[j, k]*sqrt(n)/(2*sigma[j, k]*T[j, j]*T[k, k])
+        
+        }
+      }
+
+
+      #p-value
+      p<-matrix(0,d,d)
+      p<-apply(W_n, 1, function(x) 2*(1 - pnorm(abs(x))))
+      rm(temp1,temp2,temp3,temp4,T_W,W_n)
+    }
+    rm(R,F,G,w,T_k)
+  }
+  
+  error=0
+  for(j in 1:d)
+  {
+    for(k in 1:d)
+    {
+      if(j==k)
+        next
+      if(p[j, k]<alpha && adj[j, k]==0){
+        error=error+1
+      }
+    }
+  }
+  error=error/d^2
+  
+  
+  inf = list()
+  inf$data = data
+  inf$p = p
+  inf$error = error
+  
+  rm(U,p)
+  return(inf)
+}
+  
+  
--- README.md
+++ README.md
@@ -1,13 +1,132 @@
-# huge
-High-Dimensional Undirected Graph Estimation
+<h1 align="center">Huge</h1>
+<h4 align="center">R Package for High-Dimensional Undirected Graph Estimation and Inference</h4>
+
+___Huge___ (Huge-Dimensional Undirencted Graph Estimation) implements the algorithm of estimating the parameters of a Gaussian distribution in such a way that the resulting undirected graphical model is sparse. The core algorithm is implemented in C++ with RcppEigen support for portable high performance linear algebra. It also implements a unified framework to quantify local and global inferential uncertainty for high dimensional graphical models. In particular, we consider the problems of testing the presence of a single edge. Runtime profiling is documented in the [__Performance__](#performance) section.
+
 
 ## Installation
+### Installing from GitHub
 
-## Reference
-[1] [High-dimensional Undirected
-Graph Estimation](https://cran.r-project.org/web/packages/huge/vignettes/vignette.pdf)  
-[2] [Quanquan Gu, Yuan Cao, et al. Local and Global Inference for High Dimensional Nonparanormal Graphical Models](https://arxiv.org/abs/1502.02347)  
-[3] [Picasso: A Sparse Learning Library for High Dimensional Data Analysis in R and Python](https://cran.r-project.org/web/packages/picasso/vignettes/vignette.pdf)
+First, you need to install the devtools package. You can do this from CRAN. Invoke R and then type
+
+```
+install.packages(devtools)
+```
+
+Then load the devtools package and install SAM
+
+```
+library(devtools)
+install_github("HMJiangGatech/huge")
+library(huge)
+```
+
+*Windows User:*  If you enconter a Rtools version issue: 1. make sure you install the latest [Rtools](https://cran.r-project.org/bin/windows/Rtools/); 2. try the following code
+```R
+assignInNamespace("version_info", c(devtools:::version_info, list("3.5" = list(version_min = "3.3.0", version_max = "99.99.99", path = "bin"))), "devtools")
+```
+
+### Install from CRAN (not up to date yet)
+
+Ideally you can just install and enable SAM using with the help of CRAN on an R console, but we haven't uploaded the package to CRAN yet, so this method is not available so far.
+
+```
+install.packages("huge")
+library(huge)
+```
+
+## Examples
+
+```R
+#generate data  
+L = huge.generator(n = 50, d = 12, graph = "hub", g = 4)
+
+#graph path estimation using glasso  
+est = huge(L$data, method = "glasso")
+plot(est)
+
+#inference of Gaussian graphical model at 0.05 significance level  
+T = est$icov[[10]]  
+inf = huge.inference(L$data, T, L$theta)
+print(inf$error) # print out type-I error
+```
+
+## Experiments
+For detailed implementation of the experiments, please refer to `tests/benchmark.R`
+
+### Graph Estimation
+
+We compared our package on hub graph with (n=50,d=12) with other packages, namely, grplasso, grpreg and gglasso.
+Huge significantly outperforms clime, QUIC and original huge in timing performance.
+
+<center>
+<table>
+  <thead>
+    <tr>
+      <th></th>
+      <th>CPU Times(s)</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><center>Huge</center></td>
+      <td><center>1.12</center></td>
+    </tr>
+    <tr>
+      <td><center>Huge v1.2.7</center></td>
+      <td><center>1.80</center></td>
+    </tr>
+	<tr>
+	  <td><center>QUIC</center></td>
+      <td><center>7.50</center></td>
+	</tr>
+	<tr>
+	  <td><center>Clime</center></td>
+      <td><center>416.77</center></td>
+	</tr>
+  </tbody>
+</table>
+</center>
+
+### Graph Inference.
+When using the Gaussian graphical model, huge controls the type I error well.
+
+<center>
+<table>
+    <tr>
+	  <td></td>
+	  <td colspan = "2"><center>band</center></td>
+	  <td colspan = "2"><center>hub</center></td>
+	  <td colspan = "2"><center>scale-free</center></td>
+	<tr>
+      <td><center>significance level</center></td>
+      <td><center>0.05</center></td>
+      <td><center>0.10</center></td>
+	  <td><center>0.05</center></td>
+      <td><center>0.10</center></td>
+      <td><center>0.05</center></td>
+      <td><center>0.10</center></td>
+    </tr>
+    <tr>
+      <td><center>type I error</center></td>
+	  <td><center>0.0175</center></td>
+	  <td><center>0.0391</center></td>
+      <td><center>0.0347</center></td>
+	  <td><center>0.0669</center></td>
+	  <td><center>0.0485</center></td>
+      <td><center>0.0854</center></td>
+	</tr>
+  </tbody>
+</table>
+</center>
+
+## References
+[1] [T. Zhao and H. Liu, The huge Package for High-dimensional Undirected Graph Estimation in R, 2012](https://cran.r-project.org/web/packages/huge/vignettes/vignette.pdf)  
+[2] [Xingguo Li, Jason Ge, Haoming Jiang, Mingyi Hong, Mengdi Wang, and Tuo Zhao, Boosting Pathwise Coordiante Optimization: Sequential Screening and Proximal Subsampled Newton Subroutine, 2016](https://www2.isye.gatech.edu/~tzhao80/)  
+[3] [Quanquan Gu, Yuan Cao, et al. Local and Global Inference for High Dimensional Nonparanormal Graphical Models](https://arxiv.org/abs/1502.02347)  
+[4] [Conﬁdence intervals for high-dimensional inverse covariance estimation](https://projecteuclid.org/download/pdfview_1/euclid.ejs/1433195859)  
+[5] D. Witten and J. Friedman, New insights and faster computations for the graphical lasso,2011  
+[6] N. Meinshausen and P. Buhlmann, High-dimensional Graphs and Variable Selection with the Lasso, 2006
 
 ## GSOC TODO List
 
@@ -22,10 +141,10 @@ Graph Estimation](https://cran.r-project.org/web/packages/huge/vignettes/vignett
 - [x] Implement Active Set Newton solver.
 
 ### Inference Module
-- [ ] Add inference module
+- [x] Add inference module
 
 ### Polish and submit
-- [ ] Polish `readme.md`
-- [ ] Polish R Documents
+- [x] Polish `readme.md`
+- [x] Polish R Documents
 - [ ] Polish `vignette`
 - [ ] submit to CRAN
